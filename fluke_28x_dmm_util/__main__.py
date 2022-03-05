7b5090c0c711c7d226533a5f5d7b7ef73dce6810
nbenm <nb@dagami.org>
2022-03-05 13:04:00 +0100

    real v0.2.4

--- fluke_28x_dmm_util/__main__.py
+++ fluke_28x_dmm_util/__main__.py
@@ -6,9 +6,9 @@ from fluke_28x_dmm_util import dmm_util
 
 def cli():
   if sys.version_info[0] < 3 or sys.version_info[1] < 10:
-  then
-    print ('Python version 3.10 or later is needed. Please consider upgrading')
-    sys.exit(1)
+    then
+      print ('Python version 3.10 or later is needed. Please consider upgrading')
+      sys.exit(1)
   fi
   """Call the utility"""
   dmm_util.main()
